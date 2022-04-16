2e9da60703121a5222e26b62c09b3764e1f1f1a4
Fragly <56320839+FraglyGame@users.noreply.github.com>
2022-04-17 01:55:49 +0200

    Add files via upload

--- 2.0/source/code/main.js
+++ 2.0/source/code/main.js
@@ -1,11 +1,23 @@
 var secondImgActive = false;
 
+String.prototype.replaceAt = function(index, replacement) {
+    if (index >= this.length) {
+        return this.valueOf();
+    }
+
+    var chars = this.split('');
+    chars[index] = replacement;
+    return chars.join('');
+}
+
 function onScroll() {
   // first image \\
   var yOff = window.pageYOffset;
+  var scrollPercentage = (yOff/window.innerHeight);
   var img = document.getElementById("firstImg");
   var img2 = document.getElementById("secondImg");
   var mheader = document.getElementById("mainHeader");
+  var otherInf = document.getElementById("otherInfo");
 
   if (yOff != 0) {
     // removes first image and brings up second image \\
@@ -15,6 +27,13 @@ function onScroll() {
     img2.classList.add("secondImgOn");
 
     mheader.classList.remove("active")
+
+    if (secondImgActive == false)  {
+      transition(true, "Fragly", [document.getElementById("FraglyTitle2")]);
+      setTimeout(function(){transition(true, "Fragly", [document.getElementById("FraglyTitle2")])},500)
+
+      setTimeout(function(){transition(false, "Fragly", [document.getElementById("FraglyTitle2")])},1000)
+    }
 
     secondImgActive = true;
   } else {
@@ -26,20 +45,68 @@ function onScroll() {
 
     mheader.classList.remove("active")
 
+    if (secondImgActive == true)  {
+      setTimeout(function(){
+        transition(true, "theFragly.com", [document.getElementById("title1"),document.getElementById("title2")]);
+        setTimeout(function(){transition(false, "theFragly.com", [document.getElementById("title1"),document.getElementById("title2")])},300)
+      }, 1000)
+    }
+
     secondImgActive = false;
   }
   // second image \\
-  if ((yOff/window.innerHeight) >= 0.6) { //  && secondImgActive == false)
+  if (scrollPercentage >= 0.6) {
     // removes second image and brings up the other page \\
     img.classList.remove("offScreen");
 
     img2.classList.remove("secondImgOn");
     img2.classList.add("secondImgOff");
 
+    otherInf.classList.remove("hidden")
+
     secondImgActive == false;
 
     mheader.classList.add("active");
+  } else {
+    otherInf.classList.add("hidden")
   }
 }
 
 window.addEventListener("scroll", onScroll);
+
+function transition(setup, defaultString, elements) {
+  let characters = ['!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0','1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','[','[','\\',']',']','^','_','`','░','▒','▓','│','┤','╡','╢','╖','╕','╣','║','╗','╝','╜','╛','┐','└','┴','┬','├','─','┼','╞','╟','╚','╔','╩','╦','╠','═','╬','╧','╨','╤','╥','╙','╘','╒','╓','╫','╪','┘','┌','█','▄','▌','▐','▀','α','ß','Γ','π','Σ','σ','µ','τ','Φ','Θ','Ω','δ','∞','φ','ε','∩','≡','±','≥','≤','⌠','⌡','≈','°','·','·','√','ⁿ','■'];
+
+  function changeElements(newText) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = newText;
+    }
+  }
+
+  function set(at){
+    if (setup==true) {
+      var char = characters[Math.floor(characters.length*Math.random())];
+      var nStr = elements[0].innerHTML.replaceAt(at, char);
+      changeElements(nStr);
+    } else {
+      var nStr = elements[0].innerHTML.replaceAt(at, defaultString[at]);
+      changeElements(nStr);
+    }
+  }
+
+  var i = 0;
+  function myLoop() {
+    setTimeout(function() {
+      set(i);
+      i++;
+      if (i < defaultString.length) {
+        myLoop();
+      }
+    }, 50)
+  }
+  myLoop();
+  if (setup == false) { setTimeout(function() { changeElements(defaultString); }, defaultString.length*50); };
+}
+
+transition(true, "theFragly.com", [document.getElementById("title1"),document.getElementById("title2")]);
+setTimeout(function(){transition(false, "theFragly.com", [document.getElementById("title1"),document.getElementById("title2")])},300)
